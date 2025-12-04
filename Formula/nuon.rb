a7cca239b0a3e54094b9fdb2713b9955a4a3096f
Amit Meena <29653282+im-Amitto@users.noreply.github.com>
2025-12-05 01:23:43 +0530

    feat: lsp (#462)
    * lsp
    
    * add lsp

--- Formula/nuon.rb
+++ Formula/nuon.rb
@@ -1,8 +1,9 @@
 class Nuon < Formula
-  desc "CLI client for Nuon"
+  desc "CLI client for Nuon with Language Server Protocol support"
   homepage "https://www.nuon.co/"
   version "0.19.712"
 
+  # CLI binary
   if OS.mac? && Hardware::CPU.intel?
     url "https://nuon-artifacts.s3.us-west-2.amazonaws.com/cli/0.19.712/nuon_darwin_amd64"
     sha256 "8a968d5a171dcbaac4ded0aebea9ce293de165fd160dbaf383b9e0f8574b1e6d"
@@ -28,25 +29,72 @@ class Nuon < Formula
     sha256 "3a3e089c3b83f9f9196464c26f6bb3729bb2539b5b284e72633095de05d84951"
   end
 
+  # LSP binary (as a resource)
+  if OS.mac? && Hardware::CPU.intel?
+    resource "lsp" do
+      url "https://nuon-artifacts.s3.us-west-2.amazonaws.com/lsp/0.19.712/nuon-lsp_darwin_amd64"
+      sha256 "f0bcb1557c8c06b480ea512bf0e24dc7d5a7bcfae695ef53fe4dad5d0ec9eb8e"
+    end
+  end
+
+  if OS.mac? && Hardware::CPU.arm?
+    resource "lsp" do
+      url "https://nuon-artifacts.s3.us-west-2.amazonaws.com/lsp/0.19.712/nuon-lsp_darwin_arm64"
+      sha256 "77eb58c5fa194157969ef1f6cebf189044652837ef5812e948142d2141dd260b"
+    end
+  end
+
+  if OS.linux? && Hardware::CPU.intel?
+    resource "lsp" do
+      url "https://nuon-artifacts.s3.us-west-2.amazonaws.com/lsp/0.19.712/nuon-lsp_linux_amd64"
+      sha256 "d4d468731ebc4f3c01bb5260484d3699fd5ac3ea8b771d3644f8a11e48920801"
+    end
+  end
+
+  if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
+    resource "lsp" do
+      url "https://nuon-artifacts.s3.us-west-2.amazonaws.com/lsp/0.19.712/nuon-lsp_linux_arm"
+      sha256 "bffd53fac0722e6c02ee65ac632ce1da4ca7f98b6979f8eca0545b5d6b1ff1e8"
+    end
+  end
+
+  if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+    resource "lsp" do
+      url "https://nuon-artifacts.s3.us-west-2.amazonaws.com/lsp/0.19.712/nuon-lsp_linux_arm64"
+      sha256 "8534bfd69a421a43e590f8f07cb3cb8e44ce073eacde495554f87863d215615b"
+    end
+  end
+
   def install
-    # Clunky way to get filename.
+    # Determine CLI binary filename based on platform
     if OS.mac? && Hardware::CPU.intel?
-      filename = "nuon_darwin_amd64"
+      cli_filename = "nuon_darwin_amd64"
+      lsp_filename = "nuon-lsp_darwin_amd64"
     elsif OS.mac? && Hardware::CPU.arm?
-      filename = "nuon_darwin_arm64"
+      cli_filename = "nuon_darwin_arm64"
+      lsp_filename = "nuon-lsp_darwin_arm64"
     elsif OS.linux? && Hardware::CPU.intel?
-      filename = "nuon_linux_amd64"
+      cli_filename = "nuon_linux_amd64"
+      lsp_filename = "nuon-lsp_linux_amd64"
     elsif OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-      filename = "nuon_linux_arm"
+      cli_filename = "nuon_linux_arm"
+      lsp_filename = "nuon-lsp_linux_arm"
     elsif OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      filename = "nuon_linux_arm64"
+      cli_filename = "nuon_linux_arm64"
+      lsp_filename = "nuon-lsp_linux_arm64"
     end
 
-    # Need to rename the file because we're downloading a binary.
-    bin.install filename => "nuon"
+    # Install CLI binary
+    bin.install cli_filename => "nuon"
+
+    # Install LSP binary from resource
+    resource("lsp").stage do
+      bin.install lsp_filename => "nuon-lsp"
+    end
   end
 
   test do
     system "#{bin}/nuon", "version"
+    system "#{bin}/nuon-lsp", "--help"
   end
 end
--- scripts/generate.sh
+++ scripts/generate.sh
@@ -2,66 +2,166 @@
 # shellcheck disable=SC2086,SC2046,SC2250,SC2312
 
 version=$1
-artifact_url="https://nuon-artifacts.s3.us-west-2.amazonaws.com/cli/$version"
+cli_artifact_url="https://nuon-artifacts.s3.us-west-2.amazonaws.com/cli/$version"
+lsp_artifact_url="https://nuon-artifacts.s3.us-west-2.amazonaws.com/lsp/$version"
 
-checksum_file=$(curl -s "$artifact_url/checksums.txt")
-nuon_darwin_amd64_checksum=$(echo "$checksum_file" | grep "nuon_darwin_amd64$" | cut -b -64)
-nuon_darwin_arm64_checksum=$(echo "$checksum_file" | grep "nuon_darwin_arm64$" | cut -b -64)
-nuon_linux_amd64_checksum=$(echo "$checksum_file" | grep "nuon_linux_amd64$" | cut -b -64)
-nuon_linux_arm_checksum=$(echo "$checksum_file" | grep "nuon_linux_arm$" | cut -b -64)
-nuon_linux_arm64_checksum=$(echo "$checksum_file" | grep "nuon_linux_arm64$" | cut -b -64)
+# Fetch CLI checksums
+cli_checksum_file=$(curl -s "$cli_artifact_url/checksums.txt")
+nuon_darwin_amd64_checksum=$(echo "$cli_checksum_file" | grep "nuon_darwin_amd64$" | cut -b -64)
+nuon_darwin_arm64_checksum=$(echo "$cli_checksum_file" | grep "nuon_darwin_arm64$" | cut -b -64)
+nuon_linux_amd64_checksum=$(echo "$cli_checksum_file" | grep "nuon_linux_amd64$" | cut -b -64)
+nuon_linux_arm_checksum=$(echo "$cli_checksum_file" | grep "nuon_linux_arm$" | cut -b -64)
+nuon_linux_arm64_checksum=$(echo "$cli_checksum_file" | grep "nuon_linux_arm64$" | cut -b -64)
+
+# Fetch LSP checksums (if available)
+lsp_checksum_file=$(curl -s "$lsp_artifact_url/checksums.txt")
+if echo "$lsp_checksum_file" | grep -q "nuon-lsp"; then
+  has_lsp=true
+  lsp_darwin_amd64_checksum=$(echo "$lsp_checksum_file" | grep "nuon-lsp_darwin_amd64$" | cut -b -64)
+  lsp_darwin_arm64_checksum=$(echo "$lsp_checksum_file" | grep "nuon-lsp_darwin_arm64$" | cut -b -64)
+  lsp_linux_amd64_checksum=$(echo "$lsp_checksum_file" | grep "nuon-lsp_linux_amd64$" | cut -b -64)
+  lsp_linux_arm_checksum=$(echo "$lsp_checksum_file" | grep "nuon-lsp_linux_arm$" | cut -b -64)
+  lsp_linux_arm64_checksum=$(echo "$lsp_checksum_file" | grep "nuon-lsp_linux_arm64$" | cut -b -64)
+  echo "✅ LSP artifacts found for version $version"
+else
+  has_lsp=false
+  echo "⚠️  LSP artifacts not found for version $version, skipping LSP installation"
+fi
+
+# Generate formula with conditional LSP support
+if [ "$has_lsp" = true ]; then
+  desc_text="CLI client for Nuon with Language Server Protocol support"
+else
+  desc_text="CLI client for Nuon"
+fi
 
 cat >./Formula/nuon.rb <<EOL
 class Nuon < Formula
-  desc "CLI client for Nuon"
+  desc "${desc_text}"
   homepage "https://www.nuon.co/"
   version "${version}"
 
+  # CLI binary
   if OS.mac? && Hardware::CPU.intel?
-    url "${artifact_url}/nuon_darwin_amd64"
+    url "${cli_artifact_url}/nuon_darwin_amd64"
     sha256 "${nuon_darwin_amd64_checksum}"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "${artifact_url}/nuon_darwin_arm64"
+    url "${cli_artifact_url}/nuon_darwin_arm64"
     sha256 "${nuon_darwin_arm64_checksum}"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "${artifact_url}/nuon_linux_amd64"
+    url "${cli_artifact_url}/nuon_linux_amd64"
     sha256 "${nuon_linux_amd64_checksum}"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "${artifact_url}/nuon_linux_arm"
+    url "${cli_artifact_url}/nuon_linux_arm"
     sha256 "${nuon_linux_arm_checksum}"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "${artifact_url}/nuon_linux_arm64"
+    url "${cli_artifact_url}/nuon_linux_arm64"
     sha256 "${nuon_linux_arm64_checksum}"
   end
+EOL
+
+# Add LSP resources if available
+if [ "$has_lsp" = true ]; then
+  cat >>./Formula/nuon.rb <<EOL
+
+  # LSP binary (as a resource)
+  if OS.mac? && Hardware::CPU.intel?
+    resource "lsp" do
+      url "${lsp_artifact_url}/nuon-lsp_darwin_amd64"
+      sha256 "${lsp_darwin_amd64_checksum}"
+    end
+  end
+
+  if OS.mac? && Hardware::CPU.arm?
+    resource "lsp" do
+      url "${lsp_artifact_url}/nuon-lsp_darwin_arm64"
+      sha256 "${lsp_darwin_arm64_checksum}"
+    end
+  end
+
+  if OS.linux? && Hardware::CPU.intel?
+    resource "lsp" do
+      url "${lsp_artifact_url}/nuon-lsp_linux_amd64"
+      sha256 "${lsp_linux_amd64_checksum}"
+    end
+  end
+
+  if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
+    resource "lsp" do
+      url "${lsp_artifact_url}/nuon-lsp_linux_arm"
+      sha256 "${lsp_linux_arm_checksum}"
+    end
+  end
+
+  if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+    resource "lsp" do
+      url "${lsp_artifact_url}/nuon-lsp_linux_arm64"
+      sha256 "${lsp_linux_arm64_checksum}"
+    end
+  end
+EOL
+fi
+
+# Add install method
+cat >>./Formula/nuon.rb <<EOL
 
   def install
-    # Clunky way to get filename.
+    # Determine CLI binary filename based on platform
     if OS.mac? && Hardware::CPU.intel?
-      filename = "nuon_darwin_amd64"
+      cli_filename = "nuon_darwin_amd64"
+      lsp_filename = "nuon-lsp_darwin_amd64"
     elsif OS.mac? && Hardware::CPU.arm?
-      filename = "nuon_darwin_arm64"
+      cli_filename = "nuon_darwin_arm64"
+      lsp_filename = "nuon-lsp_darwin_arm64"
     elsif OS.linux? && Hardware::CPU.intel?
-      filename = "nuon_linux_amd64"
+      cli_filename = "nuon_linux_amd64"
+      lsp_filename = "nuon-lsp_linux_amd64"
     elsif OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-      filename = "nuon_linux_arm"
+      cli_filename = "nuon_linux_arm"
+      lsp_filename = "nuon-lsp_linux_arm"
     elsif OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      filename = "nuon_linux_arm64"
+      cli_filename = "nuon_linux_arm64"
+      lsp_filename = "nuon-lsp_linux_arm64"
     end
 
-    # Need to rename the file because we're downloading a binary.
-    bin.install filename => "nuon"
+    # Install CLI binary
+    bin.install cli_filename => "nuon"
+EOL
+
+# Add LSP installation if available
+if [ "$has_lsp" = true ]; then
+  cat >>./Formula/nuon.rb <<EOL
+
+    # Install LSP binary from resource
+    resource("lsp").stage do
+      bin.install lsp_filename => "nuon-lsp"
+    end
+EOL
+fi
+
+# Add test method
+cat >>./Formula/nuon.rb <<EOL
   end
 
   test do
     system "#{bin}/nuon", "version"
+EOL
+
+if [ "$has_lsp" = true ]; then
+  cat >>./Formula/nuon.rb <<EOL
+    system "#{bin}/nuon-lsp", "--help"
+EOL
+fi
+
+cat >>./Formula/nuon.rb <<EOL
   end
 end
 EOL
